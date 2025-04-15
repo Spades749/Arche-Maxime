0b219a20c22dc7d432de8111de80e839379b21f2
Spades749 <mathis.fournier74930@gmail.com>
2025-04-15 18:57:22 +0200

    aaaa

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-﻿import random
+﻿# main.py
+import random
 import pygame
 from pygame.locals import *
 from OpenGL.GL import *
@@ -12,7 +13,7 @@ import geometry
 pygame.init()
 width, height = 1280, 720
 pygame.display.set_mode((width, height), DOUBLEBUF | OPENGL)
-pygame.display.set_caption("Arche Maxim - Ciel étoilé")
+pygame.display.set_caption("Arche Maxim")
 
 glEnable(GL_DEPTH_TEST)
 glDepthFunc(GL_LESS)
@@ -25,56 +26,57 @@ gluPerspective(45.0, width / float(height), 0.1, 300.0)
 glMatrixMode(GL_MODELVIEW)
 glLoadIdentity()
 
-ark = Maxim(position=[20.0, 0.0, 0.0], radius=2.0, height=4.0)
-cam = Camera(target=ark, offset=[0.0, 5.0, 15.0])
-
 moon_radius = 5.0
 moon_position = [0.0, 0.0, -100.0]
 
+ark = Maxim(position=[20.0, 0.0, 0.0], radius=2.0, height=4.0)
+ark.set_velocity_towards(moon_position, speed=10.0)
+cam = Camera(target=ark, offset=[0.0, 5.0, 15.0])
+
+# Génération des étoiles
 stars = []
 for _ in range(200):
     r = random.uniform(50.0, 100.0)
     u = random.uniform(-1.0, 1.0)
     theta = random.uniform(0.0, 2 * mu.PI)
-    x = r * mu.sqrt(1 - u*u) * mu.cos(theta)
-    y = r * mu.sqrt(1 - u*u) * mu.sin(theta)
+    x = r * mu.sqrt(1 - u * u) * mu.cos(theta)
+    y = r * mu.sqrt(1 - u * u) * mu.sin(theta)
     z = r * u
     stars.append([x, y, z])
 
 clock = pygame.time.Clock()
-angle = 0.0
 running = True
 
 while running:
+    dt = clock.tick(60) / 1000.0
+
     for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
-        elif event.type == KEYDOWN and event.key == K_ESCAPE:
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             running = False
 
-    angle += mu.deg2rad(0.5)
-    ark.position[0] = 20.0 * mu.cos(angle)
-    ark.position[2] = 20.0 * mu.sin(angle)
+    ark.update(dt, moon_position, moon_radius)
     cam.update()
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
 
+    # Caméra
     cx, cy, cz = cam.position
     tx, ty, tz = ark.position
     gluLookAt(cx, cy, cz, tx, ty, tz, 0.0, 1.0, 0.0)
 
-    # lune
+    # Lune
     glPushMatrix()
     glTranslatef(*moon_position)
+    glRotatef(-20.0, 1.0, 0.0, 0.0)
     glColor3f(0.8, 0.8, 0.8)
     geometry.draw_sphere(moon_radius, slices=32, stacks=16)
     glPopMatrix()
 
-    # Arche
+    # Arche Maxim
     ark.draw()
 
-    # étoiles
+    # Étoiles
     glColor3f(1.0, 1.0, 1.0)
     glBegin(GL_POINTS)
     for s in stars:
@@ -82,6 +84,5 @@ while running:
     glEnd()
 
     pygame.display.flip()
-    clock.tick(60)
 
 pygame.quit()
--- maxim.py
+++ maxim.py
@@ -1,15 +1,37 @@
 ﻿from OpenGL.GL import *
 from geometry import draw_cylinder, draw_hemisphere, draw_paddle
+import maths_utils as mu
 
 class Maxim:
     def __init__(self, position, radius, height):
         self.position = list(position)
         self.radius = radius
         self.height = height
+        self.velocity = [0.0, 0.0, 0.0]
+        self.stopped = False
+
+    def set_velocity_towards(self, target, speed):
+        direction = [target[i] - self.position[i] for i in range(3)]
+        dist = mu.distance3d(self.position, target)
+        if dist == 0:
+            self.velocity = [0.0, 0.0, 0.0]
+        else:
+            factor = speed / dist
+            self.velocity = [direction[i] * factor for i in range(3)]
+
+    def update(self, dt, target, target_radius):
+        if self.stopped:
+            return
+        for i in range(3):
+            self.position[i] += self.velocity[i] * dt
+        if mu.distance3d(self.position, target) <= self.radius + target_radius:
+            print("🚀 Collision avec la lune !")
+            self.stopped = True
 
     def draw(self):
         glPushMatrix()
         glTranslatef(*self.position)
+        glRotatef(-20.0, 1.0, 0.0, 0.0)
         glColor3f(1.0, 0.8, 0.0)
 
         draw_cylinder(self.radius, self.height, slices=32)
@@ -19,19 +41,45 @@ class Maxim:
         draw_hemisphere(self.radius, slices=32, stacks=16)
         glPopMatrix()
 
-        for dx, angle in [(self.radius, 0), (-self.radius, 180)]:
+        # Visage doré
+        glPushMatrix()
+        glTranslatef(0.0, self.height / 4.0, self.radius + 0.01)
+        glColor3f(1.0, 1.0, 0.0)
+        draw_hemisphere(self.radius * 0.4, slices=16, stacks=8)
+        glPopMatrix()
+
+        # Cheminée
+        glPushMatrix()
+        glTranslatef(0.0, self.height + 1.0, 0.0)
+        glColor3f(0.6, 0.6, 0.6)
+        draw_cylinder(self.radius * 0.2, 2.0, slices=16)
+        glPopMatrix()
+
+        # Hélices
+        for side in [-1, 1]:
             glPushMatrix()
-            glTranslatef(dx, 0.0, 0.0)
-            if angle:
-                glRotatef(angle, 0.0, 1.0, 0.0)
-            draw_paddle(self.height, self.radius)
+            glTranslatef(side * (self.radius + 0.2), 0.0, -self.radius * 1.5)
+            glRotatef(90, 1.0, 0.0, 0.0)
+            glColor3f(0.2, 0.5, 1.0)
+            for i in range(8):
+                glPushMatrix()
+                glRotatef(i * 45, 0.0, 0.0, 1.0)
+                glBegin(GL_QUADS)
+                glVertex3f(-0.05, 0.0, 0.0)
+                glVertex3f( 0.05, 0.0, 0.0)
+                glVertex3f( 0.05, 0.8, 0.0)
+                glVertex3f(-0.05, 0.8, 0.0)
+                glEnd()
+                glPopMatrix()
             glPopMatrix()
 
-        for dz, angle in [(self.radius, 90), (-self.radius, -90)]:
-            glPushMatrix()
-            glTranslatef(0.0, 0.0, dz)
-            glRotatef(angle, 0.0, 1.0, 0.0)
-            draw_paddle(self.height, self.radius)
-            glPopMatrix()
+        # Rames (nombreuses)
+        for side in [-1, 1]:
+            for i in range(8):
+                glPushMatrix()
+                glTranslatef(side * self.radius, -self.height / 2 + 0.5, -2 + i * 0.6)
+                glRotatef(90 * side, 0.0, 1.0, 0.0)
+                draw_paddle(self.height * 0.6, self.radius * 0.5)
+                glPopMatrix()
 
         glPopMatrix()
